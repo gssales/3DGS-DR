d0a1a015d44a5476ea9774cc00fde92e4f63d785
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2026-03-30 06:23:22 -0300

    disable kornia

--- train.py
+++ train.py
@@ -270,7 +270,7 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_i
         tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
 
     # Report test and samples of training set
-    if iteration % testing_iterations == 0:
+    if iteration in testing_iterations:
         torch.cuda.empty_cache()
         validation_configs = ({'name': 'test', 'cameras' : scene.getTestCameras()}, 
                               {'name': 'train', 'cameras' : [scene.getTrainCameras()[idx % len(scene.getTrainCameras())] for idx in range(5, 30, 5)]})
